ed2d72894b97b368168468ba41802514921bcc1e
Rajendra Jat <jatrj18@gmail.com>
2024-05-14 18:18:18 +0530

    Final Commit:- Implementation of the game's functionalities and styling

--- src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import loaderFull from "../images/loader-full.svg";
-import banana from "../images/banana.svg";
-
-const Loader = ({ bananas }) => {
-  // Add the component logic here
-};
-
-export default Loader;
--- src/components/Modal.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Modal = ({ isOpen, onClose, children }) => {
-  // Add the component logic here
-};
-
-export default Modal;
